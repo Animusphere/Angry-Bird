b346e32324676ea6a17de560f4a6d824a95cfaad
Ricky Limbaug <54813543+Animusphere@users.noreply.github.com>
2021-11-08 20:05:07 +0700

    test
    asa

---
+++ Angry Bird/Assets/Scripts/OWL.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OWL : Bird
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Collider.radius= (float)(Collider.radius+0.5);
+    }
+}
---
+++ Angry Bird/Assets/Scripts/YellowBird.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class YellowBird : Bird
+{
+    [SerializeField]
+    public float _boostForce = 100f;
+    public bool hasBoost = false;
+
+    public void Boost()
+    {
+        if (State == BirdState.Thrown && !hasBoost)
+        {
+            RigidBody.AddForce(RigidBody.velocity * _boostForce);
+            hasBoost = true;
+        }
+    }
+    public override void OnTap()
+    {
+        Boost();
+    }
+}
